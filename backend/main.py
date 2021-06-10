9396f0c103af88a37c8a4f4ea065688bb17415e3
woriazzc <1925854083@qq.com>
2021-06-10 09:49:53 +0800

    add method, task

--- backend/main.py
+++ backend/main.py
@@ -91,21 +91,91 @@ method查询接口
 筛选某一方法的所有论文。在该筛选结果中按论文名模糊查找
 methodName: 方法名称
 title: 论文名称
+taskName
 '''
 @app.route('/method',methods= ['GET', 'POST'])
 def method():
     cur = db.cursor()
-    cur.execute("SELECT * FROM v_method_papercount ORDER BY paperCnt DESC")
+    methodName = request.args.get('methodName')
+    title = request.args.get('title')
+    secsql = ''
+    if methodName is not None:
+        secsql = 'WHERE methodName LIKE "%' + methodName + '%"'
+    if title is not None:
+        if methodName is not None:
+            secsql += 'AND title LIKE "%' + title + '%"'
+        else:
+            secsql = 'WHERE title LIKE "%' + title + '%"'
+    if secsql is None:
+        cur.execute("SELECT * FROM v_method_papercount ORDER BY paperCnt DESC")
+    else:
+        cur.execute("SELECT DISTINCT * FROM v_method_of_paper JOIN paper USING(paperId) {}".format(secsql))
     results = cur.fetchall()
     lst = []
-    for row in results:
-        lst.append({
-            "methodName": row[0],
-            "methodDesc": row[1]
-        })
+    if methodName is None and title is None:
+        for row in results:
+            lst.append({
+                "methodName": row[0],
+                "methodDesc": row[1]
+            })
+    else:
+        for row in results:
+            lst.append({
+                "methodName": row[2],
+                "title": row[3],
+                "paperLink": row[4],
+                "abs": row[5],
+                "publishDate": row[6].strftime('%Y-%m-%d')
+            })
     cur.close()
     return Response(json.dumps(lst), mimetype='application/json')
 
+'''
+task查询接口
+根据任务查询所有相关的评价指标，以及参与该评价指标中得分最高的论文
+根据任务查询所有相关的数据集
+taskName: 任务名称
+selection: bench/dataset 查询指标或数据集
+'''
+@app.route('/task',methods= ['GET', 'POST'])
+def task():
+    cur = db.cursor()
+    taskName = request.args.get('taskName')
+    selection = request.args.get('selection')
+    if selection is None:
+        cur.execute('SELECT * FROM task')
+        results = cur.fetchall()
+        lst = []
+        for row in results:
+            lst.append({
+                "taskName": row[2],
+                "taskDesc": row[1]
+            })
+    else:
+        if selection == "bench":
+            cur.execute("SELECT * FROM v_bench_of_task WHERE taskName = %s", (taskName))
+            results = cur.fetchall()
+            lst = []
+            for row in results:
+                lst.append({
+                    "metric": row[2],
+                    "datasetName": row[3],
+                    "title": row[0],
+                    "paperLink": row[1]
+                })
+        elif selection == "dataset":
+            cur.execute("SELECT d.datasetName, d.datasetDesc, d.datasetLink FROM v_bench_of_task JOIN dataset d USING(datasetName) WHERE taskName=%s", (taskName))
+            results = cur.fetchall()
+            lst = []
+            for row in results:
+                lst.append({
+                    "dataSetName": row[0],
+                    "datasetDesc": row[1],
+                    "datasetLink": row[2]
+                })
+
+    cur.close()
+    return Response(json.dumps(lst), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug = True)
