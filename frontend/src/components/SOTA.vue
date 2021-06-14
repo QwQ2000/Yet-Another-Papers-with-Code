075b938f6bf6d1e14795b8a73950e34448d85f58
woriazzc <1925854083@qq.com>
2021-06-14 23:34:09 +0800

    add vue.config.js

--- frontend/src/components/SOTA.vue
+++ frontend/src/components/SOTA.vue
@@ -1,15 +1,36 @@
 <template>
-  <h2>Browse State-of-the-Art</h2>
+  <div>
+    <h2>Browse State-of-the-Art</h2>
+    {{ msg }}
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'SOTA',
   data() {
     return {
+      msg: 'Hello World!',
     };
   },
   methods: {
+    getMessage() {
+      const path = '/api/sota';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+      console.log(this.msg);
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
